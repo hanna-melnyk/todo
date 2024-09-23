5e77377e829120d95a585bdd68b00a94e72330fd
Hanna <hanna.melnk@gmail.com>
2024-09-23 15:44:50 +0200

    initiated a project

---
+++ server/server.js
@@ -0,0 +1,26 @@
+//server.js
+
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello from the server!');
+});
+
+// You can add your MongoDB connection here
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
