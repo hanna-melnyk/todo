a205aa66f61c79c073d450e8f10ba5fce5e30c54
Hanna <hanna.melnk@gmail.com>
2024-10-06 15:09:25 +0200

    added configuration for production

--- server/server.js
+++ server/server.js
@@ -15,9 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello from the server!');
-});
+
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
@@ -31,6 +29,23 @@ app.use('/api', userRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+// Serve React frontend in production
+if (process.env.NODE_ENV === 'production') {
+    // Serve the static files from the frontend's `dist` folder
+    app.use(express.static(path.join(__dirname, 'client/dist')));
+
+    // Handle any other requests by serving the frontend's index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+    });
+}
+
+// Default root route (optional)
+app.get('/', (req, res) => {
+    res.send('Hello from the server!');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
